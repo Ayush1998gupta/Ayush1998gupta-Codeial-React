65a7912d3fb5075b60f32653dd85158b7230c4f2
Ayush <aayush199813@gmail.com>
2022-07-16 21:24:15 +0530

    navbar redefined

---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+function Navbar(props) {
+  return (
+    <nav className="nav">
+      <div className="left-div">
+        <img
+          src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+          alt="logo"
+        />
+      </div>
+      <div className="search-container">
+        <img
+          className="search-icon"
+          src="https://cdn-icons-png.flaticon.com/128/149/149852.png"
+          alt="search-icon"
+        />
+        <input placeholder="Search" />
+
+        <div className="search-results">
+          <ul>
+            <li className="search-results-row">
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/149/149071.png"
+                alt="user-dp"
+              />
+              <span>John Doe</span>
+            </li>
+            <li className="search-results-row">
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/149/149071.png"
+                alt="user-dp"
+              />
+              <span>John Doe</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="right-nav">
+        <div className="user">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/149/149071.png"
+            alt="user-dp"
+            id="user-dp"
+          />
+          <span>John Doe</span>
+        </div>
+        <div className="nav-links">
+          <ul>
+            <li>Log in</li>
+            <li>Log out</li>
+            <li>Register</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
