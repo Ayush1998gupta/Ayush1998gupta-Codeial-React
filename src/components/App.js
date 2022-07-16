8bf8bc16920bd0a1487cb937341a25854cb9b66d
Ayush <aayush199813@gmail.com>
2022-07-16 14:27:38 +0530

    Fetching Posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 
-function App() {
-  return (
-    <div className="App">
-     App
-    </div>
-  );
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/posts';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts())
+  }
+  render() {
+    console.log('PROPS', this.props);
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
+import { Provider } from 'react-redux';
 
 const store = configureStore();
-console.log('store', store.getState());
+// console.log('store', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
