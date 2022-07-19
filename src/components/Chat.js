1f4271786fa34a3692b01f1b66eef933de782f97
Ayush <aayush199813@gmail.com>
2022-07-19 22:48:57 +0530

    io socket

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import '../chat.css';
+import { connect } from 'react-redux';
 
 class Chat extends Component {
   constructor(props) {
@@ -9,7 +11,59 @@ class Chat extends Component {
       messages: [], // {content: 'some message', self: true}
       typedMessage: '',
     };
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+
+    this.socket.on('connect', function () {
+      console.log('CONNECTION ESTABLISHED');
+
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      socketConnection.on('user_joined', function (data) {
+        console.log('NE USER JOINED', data);
+      });
+    });
+
+    this.socket.on('receive_message', function (data) {
+      // add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
   render() {
     const { typedMessage, messages } = this.state;
 
@@ -17,18 +71,13 @@ class Chat extends Component {
       <div className="chat-container">
         <div className="chat-header">
           Chat
-          {/* <img
-            src="https://www.iconsdb.com/icons/preview/white/minus-5-xxl.png"
-            alt="img"
-            height={17}
-          /> */}
           <i className="fa-solid fa-xmark"></i>
         </div>
         <div className="chat-messages">
           {messages.map((message) => (
             <div
               className={
-                messages.self
+                message.self
                   ? 'chat-bubble self-chat'
                   : 'chat-bubble other-chat'
               }
@@ -49,5 +98,10 @@ class Chat extends Component {
     );
   }
 }
+function mapStateTOProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
 
-export default Chat;
+export default connect(mapStateTOProps)(Chat);
